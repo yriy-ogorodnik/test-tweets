2086caa77bcd7f87d7e7c283dcc8fbad86c602e6
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-05-19 01:20:07 +0300

    create redux

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,19 +1,64 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-
 axios.defaults.baseURL = "https://64655c79228bd07b3549241b.mockapi.io/";
 
-
 export const fetchTweets = createAsyncThunk(
-   'tweets/fetchAll',
-   async (_, thunkAPI) => {
-     try {
-       const response = await axios.get('/user');
-       return response.data;
-     } catch (e) {
-      //  toast.error('Something went wrong! :(');
-       return thunkAPI.rejectWithValue(e.message);
-     }
-   }
- );
+  "tweets/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/users");
+      return response.data;
+    } catch (e) {
+      toast.error("Something went wrong! :(");
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const fetchTweetsByPage = createAsyncThunk(
+  "tweets/fetchPages",
+  async (limit, thunkAPI) => {
+    try {
+      const response = await axios.get(`/users?page=1&limit=${limit}`);
+      return response.data;
+    } catch (e) {
+      toast.error("Something went wrong! :(");
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addFollower = createAsyncThunk(
+  "tweets/addFollower",
+  async (user, thunkAPI) => {
+    try {
+      const response = await axios.put(`/users/${user.id}`, {
+        ...user,
+        followers: user.followers + 1,
+        following: !user.following,
+      });
+      return response.data;
+    } catch (e) {
+      toast.error("Something went wrong! :(");
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteFollower = createAsyncThunk(
+  "tweets/deleteFollower",
+  async (user, thunkAPI) => {
+    try {
+      const response = await axios.put(`/users/${user.id}`, {
+        ...user,
+        followers: user.followers - 1,
+        following: !user.following,
+      });
+      return response.data;
+    } catch (e) {
+      toast.error("Something went wrong! :(");
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
--- src/redux/tweetsSlice.js
+++ src/redux/tweetsSlice.js
@@ -1,24 +1,76 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { fetchTweets } from "./operations";
+import { createSlice } from '@reduxjs/toolkit';
+import {
+  addFollower,
+  deleteFollower,
+  fetchTweets,
+  fetchTweetsByPage,
+} from './operations';
 
+ const handlePending = state => {
+  state.isLoading = true;
+};
 
+ const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+ const handleFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  state.items = action.payload;
+};
+
+ const handleFulfilledAdd = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+
+  const index = state.items.findIndex(tweet => tweet.id === action.payload.id);
+  state.items.splice(index, 1, action.payload);
+};
+
+ const handleFulfilledDelete = (state, action) => {
+  state.isLoading = false;
+  state.error = null;
+  const index = state.items.findIndex(tweet => tweet.id === action.payload.id);
+  state.items.splice(index, 1, action.payload);
+};
 
 const tweetsInitialState = {
-   items: [],
-   limit: 3,
-   isLoading: false,
-   error: null,
- };
+  items: [],
+  limit: 3,
+  isLoading: false,
+  error: null,
+};
 
+const tweetsSlice = createSlice({
+  name: 'tweets',
+  initialState: tweetsInitialState,
+  reducers: {
+    changeLimit: (state, action) => {
+      state.limit += action.payload;
+    },
+    deleteLimit: (state, action) => {
+      state.limit = action.payload;
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchTweets.fulfilled, handleFulfilled)
+      .addCase(fetchTweets.pending, handlePending)
+      .addCase(fetchTweets.rejected, handleRejected)
+      .addCase(fetchTweetsByPage.pending, handlePending)
+      .addCase(fetchTweetsByPage.fulfilled, handleFulfilled)
+      .addCase(fetchTweetsByPage.rejected, handleRejected)
+      .addCase(addFollower.fulfilled, handleFulfilledAdd)
+      .addCase(addFollower.pending, handlePending)
+      .addCase(addFollower.rejected, handleRejected)
+      .addCase(deleteFollower.fulfilled, handleFulfilledDelete)
+      .addCase(deleteFollower.pending, handlePending)
+      .addCase(deleteFollower.rejected, handleRejected);
+  },
+});
 
- const tweetsSlice = createSlice({
-   name: "tweets",
-   initialState: tweetsInitialState,
-   // Додаємо обробку зовнішніх екшенів
-   extraReducers: {
-     [fetchTasks.pending](state, action) {},
-     [fetchTasks.fulfilled](state, action) {},
-     [fetchTasks.rejected](state, action) {},
-   },
- });
- export const tasksReducer = tasksSlice.reducer;
+export const { changeLimit, deleteLimit } = tweetsSlice.actions;
+
+export default tweetsSlice.reducer;
