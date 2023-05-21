da54835464119de081c96147307d96b7e03ba5d6
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-05-21 17:29:33 +0300

    Configure Vite Config, add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
-
-import "./App.css";
-import FilterSelect from "./components/StatusFilter/StatusFilter";
-
+// import "./App.css";
+// import FilterSelect from "./components/StatusFilter/StatusFilter";
 
 import { TweetList } from "./components/TweetList/TweetList";
 
+// function App() {
+//   return (
+//     <>
+//     <FilterSelect/>
+
+//      <TweetList/>
+
+//     </>
+//   );
+// }
+
+// export default App;
+// ____________________________
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
 function App() {
   return (
-    <>
-    <FilterSelect/>
-     
-     <TweetList/>
-     
-     
-    </>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/test-tweets/"}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Hello World</h1>
+              <Link to="about">About Us</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <div>
+              <TweetList />
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/test-tweets/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/test-tweets/",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/test-tweets/";
+  }
+
+  return config;
 });
